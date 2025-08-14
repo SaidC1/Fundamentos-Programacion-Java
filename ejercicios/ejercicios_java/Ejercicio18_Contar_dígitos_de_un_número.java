4c01133a7d3c737b764d316988e5251b06d4d1d0
Irvin Said <txpro123@hotmail.com>
2025-08-13 22:21:34 -0600

    Add files via upload

---
+++ ejercicios/ejercicios_java/Ejercicio18_Contar_dígitos_de_un_número.java
@@ -0,0 +1,7 @@
+/*Pide al usuario un número entero positivo y muestra la cantidad de dígitos que tiene.
+Valida que el número sea positivo antes de continuar.
+
+Ask the user for a positive integer and display how many digits it has.
+Validate that the number is positive before continuing.
+
+*/
