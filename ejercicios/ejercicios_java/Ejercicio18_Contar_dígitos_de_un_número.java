600622fd027a952d53dcf92c78ce3920a2c22c91
Irvin Said <txpro123@hotmail.com>
2025-08-13 22:22:02 -0600

    Update Ejercicio18_Contar_dígitos_de_un_número.java

--- ejercicios/ejercicios_java/Ejercicio18_Contar_dígitos_de_un_número.java
+++ ejercicios/ejercicios_java/Ejercicio18_Contar_dígitos_de_un_número.java
@@ -5,3 +5,19 @@ Ask the user for a positive integer and display how many digits it has.
 Validate that the number is positive before continuing.
 
 */
+
+import java.util.Scanner;
+
+public class Ejercicio18_Contar_dígitos_de_un_número {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        // Valor número positivo
+        do {
+            System.out.println("Ingres un número entero positivo: ");
+            numero = sc.nextInt();
+            if (numero <= 0) {
+                System.out.println("Erro: el número debe ser positivo.");
+            }
+        } while (numero <= 0);
