2dae9597e7335c442d83ab1a3a3cefd0f1b9a161
Irvin Said <txpro123@hotmail.com>
2025-08-21 21:18:10 -0600

    Update Empleado.java

--- ejercicios/poo_java/herencia/Empleado.java
+++ ejercicios/poo_java/herencia/Empleado.java
@@ -4,7 +4,8 @@ package herencia;
 public class Empleado extends Persona {
     private double salario;
 
-    // 1) Constructor por defecto: debe llamar a supero() exprícitamente o implícitamente
+    // 1) Constructor por defecto: debe llamar a supero() exprícitamente o
+    // implícitamente
     public Empleado() {
         // Llama implícitamente a super() → Persona()
         this.salario = 0.0;
@@ -13,16 +14,22 @@ public class Empleado extends Persona {
     // 2) Constructor completo: SIEMPRE super(...) debe ir en la PRIMERA línea
     public Empleado(String nombre, int edad, double salario) {
         super(nombre, edad); // inicializa la parte Persona
-        if (salario < 0) throw new IllegalArgumentException("El salario nu puede ser negativo");
+        if (salario < 0)
+            throw new IllegalArgumentException("El salario nu puede ser negativo");
         this.salario = salario;
     }
 
-    public double getSalario() {return salario; }
+    public double getSalario() {
+        return salario;
+    }
+
     public void aumentarSalario(double porcentaje) {
-        if(porcentaje < 0) throw new IllegalArgumentException("Porcentaje inválido");
+        if (porcentaje < 0)
+            throw new IllegalArgumentException("Porcentaje inválido");
         this.salario += this.salario * (porcentaje / 100.0);
     }
-     @Override
+
+    @Override
     public String toString() {
         return "Empleado{" +
                 "nombre='" + getNombre() + "', " +
@@ -32,6 +39,3 @@ public class Empleado extends Persona {
 
     }
 }
-
-}
-
