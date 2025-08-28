d6a6ee589271a8fa067111a85f64524a80b85123
Irvin Said <txpro123@hotmail.com>
2025-08-27 22:24:28 -0600

    Update MainHerencia.java

--- ejercicios/poo_java/herencia/MainHerencia.java
+++ ejercicios/poo_java/herencia/MainHerencia.java
@@ -9,3 +9,17 @@ public class MainHerencia {
         System.out.println(p);
         System.out.println(e);
         System.out.println(g);
+
+            // Polimorfismo básico
+        Persona ref; // referencia de tipo base
+        ref = e; // apunta a un Empleado
+        System.out.println(ref); // llama al toString() de Empleado (dinámico)
+
+        // Lógica de negocio
+        e.aumentarSalario(10);
+        g.cumplirAnios();
+        System.out.println("Después de cambios:");
+        System.out.println(e);
+        System.out.println(g);
+    }
+}
